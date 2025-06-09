f361aacf33f5d6e8dc274ebaa831dab7a24b8241
Paul <github@goonie.nl>
2025-06-09 15:26:44 +0200

    Added application form to Pinia store

---
+++ src/stores/application-form.ts
@@ -0,0 +1,30 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+
+const useApplicationForm = defineStore('application-form', () => {
+    const personalInformation = {
+        applicationReason: ref<string>(
+            'Overstappen per 1-1-2023 naar Zilveren Kruis'
+        ),
+        firstName: ref<string>(),
+        middleName: ref<string>(),
+        lastName: ref<string>(),
+        gender: ref<string>(),
+        birthDate: ref<string>(),
+        citizenServiceNumber: ref<string>()
+    };
+
+    const insuranceOptions = {
+        insurance: ref<string>(),
+        termsOfPayment: ref<string>('per jaar'),
+        excessDeductible: ref<string>('€ 385 - verplicht eigen risico'),
+        additionalInsurance: ref<string>(
+            'Geen aanvullende verzekering geselecteerd'
+        ),
+        dentalInsurance: ref<string>('Geen tandartsverzekering geselecteerd')
+    };
+
+    return { personalInformation, insuranceOptions };
+});
+
+export { useApplicationForm };
