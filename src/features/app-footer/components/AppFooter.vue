9ceefe1becdf2ad533e1b915e883e5416f2a2102
Paul <github@goonie.nl>
2025-06-09 12:55:06 +0200

    Split up large file into smaller chunks

---
+++ src/features/app-footer/components/AppFooter.vue
@@ -0,0 +1,162 @@
+<template>
+    <div class="footer-placeholder">
+        <footer
+            class="footer"
+            role="contentinfo"
+        >
+            <!-- BEGIN NOINDEX -->
+            <section class="footer-section footer-section--horizontal">
+                <div class="container">
+                    <div
+                        class="d-flex justify-content-between align-items-center my-auto"
+                    >
+                        <ul
+                            class="list list--nostyle list--horiz d-flex mb-5 mb-xl-0"
+                        >
+                            <li class="list__item mb-0">
+                                <a
+                                    href="/consumenten/disclaimer"
+                                    class="list__link"
+                                    >disclaimer</a
+                                >
+                            </li>
+                            <li class="list__item mb-0">
+                                <a
+                                    href="/consumenten/privacy"
+                                    class="list__link"
+                                    >privacy</a
+                                >
+                            </li>
+                            <li class="list__item mb-0">
+                                <a
+                                    href="/consumenten/veiligheid"
+                                    class="list__link"
+                                    >veiligheid</a
+                                >
+                            </li>
+                            <li class="list__item mb-0">
+                                <a
+                                    href="/consumenten/cookies"
+                                    class="list__link"
+                                    >cookies</a
+                                >
+                            </li>
+                            <li class="list__item mb-0">
+                                <a
+                                    href="http://www.wijzeringeldzaken.nl/verzekering/"
+                                    class="list__link"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    >wijzer in geldzaken</a
+                                >
+                            </li>
+                            <li class="list__item mb-0">
+                                <a
+                                    href="https://nieuws.zilverenkruis.nl/"
+                                    class="list__link"
+                                    >pers</a
+                                >
+                            </li>
+                            <li class="list__item">
+                                <span class="surfly-button"></span>
+                            </li>
+                        </ul>
+
+                        <div
+                            class="buttons-social buttons-social--embed buttons-social--no-bg-color"
+                        >
+                            <ul class="buttons-social__list">
+                                <li class="buttons-social__listitem">
+                                    <a
+                                        href="https://www.facebook.com/zilverenkruis"
+                                        class="buttons-social__link"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        <span class="screenreader-only"></span>
+                                        <svg
+                                            class="icon buttons-social__icon"
+                                            aria-hidden="true"
+                                        >
+                                            <use
+                                                xlink:href="#sprite-facebook"
+                                            ></use>
+                                        </svg>
+                                    </a>
+                                </li>
+
+                                <li class="buttons-social__listitem">
+                                    <a
+                                        href="https://twitter.com/zilverenkruis"
+                                        class="buttons-social__link"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        <span class="screenreader-only"></span>
+                                        <svg
+                                            class="icon buttons-social__icon"
+                                            aria-hidden="true"
+                                        >
+                                            <use
+                                                xlink:href="#sprite-twitter"
+                                            ></use>
+                                        </svg>
+                                    </a>
+                                </li>
+
+                                <li class="buttons-social__listitem">
+                                    <a
+                                        href="https://samen.zilverenkruis.nl/"
+                                        class="buttons-social__link"
+                                    >
+                                        <span class="screenreader-only"></span>
+                                        <svg
+                                            class="icon buttons-social__icon"
+                                            aria-hidden="true"
+                                        >
+                                            <use
+                                                xlink:href="#sprite-community"
+                                            ></use>
+                                        </svg>
+                                    </a>
+                                </li>
+
+                                <li class="buttons-social__listitem">
+                                    <a
+                                        href="https://www.youtube.com/user/zilverenkruisachmea"
+                                        class="buttons-social__link"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        <span class="screenreader-only"></span>
+                                        <svg
+                                            class="icon buttons-social__icon"
+                                            aria-hidden="true"
+                                        >
+                                            <use
+                                                xlink:href="#sprite-youtube"
+                                            ></use>
+                                        </svg>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div
+                            class="text__branding text-center text-md-right pb-4 pb-md-0"
+                        >
+                            <span>Zilveren Kruis is onderdeel van</span>
+                            <svg
+                                class="icon footer__label-icon"
+                                alt="Logo Achmea"
+                            >
+                                <use xlink:href="#sprite-achmea"></use>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- END NOINDEX -->
+        </footer>
+    </div>
+</template>
