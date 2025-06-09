9de59e04cdd0af453dd0d6156045b2c1459aab9b
Paul <github@goonie.nl>
2025-06-09 16:50:12 +0200

    Added computed fields to calculate prices

--- src/stores/application-form.ts
+++ src/stores/application-form.ts
@@ -1,5 +1,21 @@
 import { defineStore } from 'pinia';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
+
+function calculateCost(
+    price: number,
+    term: 'yearly' | 'quarterly' | 'monthly'
+) {
+    switch (term) {
+        case 'monthly':
+            return `${(price / 12).toLocaleString('nl', { style: 'currency', currency: 'EUR' })} per maand`;
+
+        case 'quarterly':
+            return `${(price / 4).toLocaleString('nl', { style: 'currency', currency: 'EUR' })} per kwartaal`;
+
+        default:
+            return `${price.toLocaleString('nl', { style: 'currency', currency: 'EUR' })} per jaar`;
+    }
+}
 
 const useApplicationForm = defineStore('application-form', () => {
     const personalInformation = {
@@ -24,7 +40,90 @@ const useApplicationForm = defineStore('application-form', () => {
         dentalInsurance: ref<string>('Geen tandartsverzekering geselecteerd')
     };
 
-    return { personalInformation, insuranceOptions };
+    const computedValues = {
+        insuranceCost: computed(() => {
+            switch (insuranceOptions.insurance.value) {
+                case 'Basis Budget':
+                    return calculateCost(
+                        1393.26,
+                        insuranceOptions.paymentTerm.value
+                    );
+
+                case 'Basis Zeker':
+                    return calculateCost(
+                        1483.54,
+                        insuranceOptions.paymentTerm.value
+                    );
+
+                case 'Basis Exclusief (Restitutie)':
+                    return calculateCost(
+                        1624.62,
+                        insuranceOptions.paymentTerm.value
+                    );
+
+                default:
+                    return undefined;
+            }
+        }),
+
+        additionalInsuranceCost: computed(() => {
+            switch (insuranceOptions.additionalInsurance.value) {
+                case 'Aanvullend 1':
+                    return calculateCost(
+                        21.38,
+                        insuranceOptions.paymentTerm.value
+                    );
+
+                case 'Aanvullend 2':
+                    return calculateCost(
+                        85.06,
+                        insuranceOptions.paymentTerm.value
+                    );
+
+                case 'Aanvullend 3':
+                    return calculateCost(
+                        198.63,
+                        insuranceOptions.paymentTerm.value
+                    );
+
+                case 'Aanvullend 4':
+                    return calculateCost(
+                        359.73,
+                        insuranceOptions.paymentTerm.value
+                    );
+
+                default:
+                    return undefined;
+            }
+        }),
+
+        dentalInsuranceCost: computed(() => {
+            switch (insuranceOptions.dentalInsurance.value) {
+                case 'Tand 1':
+                    return calculateCost(
+                        80.28,
+                        insuranceOptions.paymentTerm.value
+                    );
+
+                case 'Tand 2':
+                    return calculateCost(
+                        221.65,
+                        insuranceOptions.paymentTerm.value
+                    );
+
+                case 'Tand 3':
+                    return calculateCost(
+                        449.36,
+                        insuranceOptions.paymentTerm.value
+                    );
+
+                default:
+                    return undefined;
+            }
+        })
+    };
+
+    return { personalInformation, insuranceOptions, computedValues };
 });
 
 export { useApplicationForm };
